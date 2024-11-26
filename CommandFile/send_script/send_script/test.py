8c48856f25a0356c6edd445194ce382a66d41954
JAJAYU46 <125646177+JAJAYU46@users.noreply.github.com>
2024-11-27 00:34:10 +0800

    Add files via upload

---
+++ CommandFile/send_script/send_script/test.py
@@ -0,0 +1,12 @@
+from math import sqrt, atan2
+
+yaw = atan2(100.00, 300.00)
+pitch = atan2(500.00, sqrt(300.00**2 + 100**2))
+roll = 0.00
+
+# targetP1 = f"{x_base}, {y_base}, {z_base}, -180.00, 0.0, 135.00"
+            
+
+
+
+            #targetP1 = "300.00, 100, 500, -180.00, 0.0, 135.00"
